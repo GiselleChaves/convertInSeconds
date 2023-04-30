e8af610aa841ff0545f8ec667cc7b5b62e1fbcb8
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-04-30 14:10:29 -0300

    Add files via upload

---
+++ convertInSeconds.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+  /**
+   * 4. Crie um programa que lê o tempo atual em hora, minuto e segundo, e o escreve em segundos 
+   * quanto tempo se passou desde que o dia começou. Essa conversão deve ser calculada por um 
+   * método.
+   */
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int hours, minuts, seconds, hoursInSeconds;
+
+    System.out.println("Informe o tempo atual com hora,minutos e segundas separadamente:");
+    hours = in.nextInt();
+    minuts = in.nextInt();
+    seconds = in.nextInt();
+
+    hoursInSeconds = convertInSeconds(hours, minuts, seconds);
+    System.out.printf("O tempo atual convertido em segundos é: %d.", hoursInSeconds);
+    System.out.println();
+    System.out.printf("\n.::Fim da Execução::.");
+    in.close();
+  }
+  public static int convertInSeconds(int n1,int n2, int n3){
+    return ((n1 * 60) + (n2 * 60) + n3);
+  }
+}
